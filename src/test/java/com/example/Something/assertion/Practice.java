808dd4d43e12b1ea95f42e82390f5a8d801d2dac
ddalam <dychoi.dev@gmail.com>
2021-05-29 16:02:50 +0900

    오브젝트 테스트

---
+++ src/test/java/com/example/Something/assertion/Practice.java
@@ -0,0 +1,27 @@
+package com.example.Something.assertion;
+
+import org.junit.jupiter.api.Test;
+
+// Assertion은 아래 import문을 통해
+import static org.assertj.core.api.Assertions.*;
+
+public class Practice {
+
+    @Test
+    void test() {
+        // 어떤 것도 검증하지 않으며, 테스트에 실패하지 않는다
+        assertThat("aa");
+    }
+
+    @Test
+    void compareObjects() {
+        Dog fido = new Dog("Fido", 5.25F);
+        Dog fidosClone = new Dog("Fido", 5.25F);
+
+        // isEqualTo()는 객체의 참조를 비교하기 때문에 실패
+        assertThat(fido).isEqualTo(fidosClone);
+
+        // 콘텐츠의 동등성을 비교하려면 isEqualToComparingFieldByFieldRecursively()를 사용
+        assertThat(fido).isEqualToComparingFieldByFieldRecursively(fidosClone);
+    }
+}
