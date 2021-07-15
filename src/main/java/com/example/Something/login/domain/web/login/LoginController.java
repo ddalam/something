631bc461f1efc76e95acf5f2181e24a15f1b9c27
ddalam <dychoi.dev@gmail.com>
2021-07-15 13:45:10 +0900

    로그인 후 response에 쿠키 추가

--- src/main/java/com/example/Something/login/domain/web/login/LoginController.java
+++ src/main/java/com/example/Something/login/domain/web/login/LoginController.java
@@ -5,10 +5,16 @@ import com.example.Something.login.domain.member.Member;
 import com.example.Something.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -20,7 +26,7 @@ public class LoginController {
 	private final MemberRepository memberRepository;
 
 	@PostMapping("/login")
-	public String login(@Valid @RequestBody LoginForm loginForm) {
+	public String login(@Valid @RequestBody LoginForm loginForm, HttpServletResponse response) {
 
 		Member newMember = new Member();
 		newMember.setLoginId(loginForm.getLoginId());
@@ -34,7 +40,17 @@ public class LoginController {
 			return "아이디 또는 비밀번호가 맞지 않습니다";
 		}
 
-		// TODO 로그인 성공 처리
+		// 로그인 성공
+
+		// 쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료 시 모두 종료)
+		Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+		response.addCookie(idCookie);
+
 		return "로그인 성공";
+	}
+
+	@GetMapping("/temp")
+	public String temp() {
+		return "abc";
 	}
 }
