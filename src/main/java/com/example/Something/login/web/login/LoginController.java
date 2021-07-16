6e7675efbc6db0aa19d167390e636ca793522a98
ddalam <dychoi.dev@gmail.com>
2021-07-16 10:26:04 +0900

    디렉토리 위치 변경

--- src/main/java/com/example/Something/login/web/login/LoginController.java
+++ src/main/java/com/example/Something/login/web/login/LoginController.java
@@ -1,13 +1,10 @@
-package com.example.Something.login.domain.web.login;
+package com.example.Something.login.web.login;
 
 import com.example.Something.login.domain.login.LoginService;
 import com.example.Something.login.domain.member.Member;
 import com.example.Something.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
