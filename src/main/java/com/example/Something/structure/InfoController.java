6d0905c1bc443ff787eeb0f139295e90b26aedfa
ddalam <dychoi.dev@gmail.com>
2021-11-28 00:52:19 +0900

    상위 클래스 타입 리스트로 리턴

--- src/main/java/com/example/Something/structure/InfoController.java
+++ src/main/java/com/example/Something/structure/InfoController.java
@@ -1,5 +1,7 @@
 package com.example.Something.structure;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,24 @@ public class InfoController {
 				.year(10)
 				.month(4)
 				.build().toString();
+	}
+
+	@GetMapping("/infoList")
+	public List<Info> getInfoList() {
+		MonthlyInfo a = MonthlyInfo.builder()
+				.year(10)
+				.month(4)
+				.build();
+
+		MonthlyInfo b = MonthlyInfo.builder()
+				.year(10)
+				.month(4)
+				.build();
+
+		List<Info> c = new ArrayList<>();
+		c.add(a);
+		c.add(b);
+
+		return c;
 	}
 }
