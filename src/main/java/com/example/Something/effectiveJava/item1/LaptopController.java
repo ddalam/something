ae46c218336a3f0c223895c6f2e7164ec69a22a0
ddalam <dychoi.dev@gmail.com>
2021-11-28 12:32:29 +0900

    디렉토리 이동, readme 추가

--- src/main/java/com/example/Something/effectiveJava/item1/LaptopController.java
+++ src/main/java/com/example/Something/effectiveJava/item1/LaptopController.java
@@ -1,4 +1,4 @@
-package com.example.Something.staticFactoryMethod;
+package com.example.Something.effectiveJava.item1;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
